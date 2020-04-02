067590eab4cb84b239afb82aedc07efccc0a2a08
hongfei.zhang <hongfei.zhang@ly.com>
2020-04-02 11:16:22 +0800

    提交实体类模块

---
+++ src/main/java/com/module/UserEntity.java
@@ -0,0 +1,43 @@
+package com.module;
+
+/**
+ * Created by hongfei.zhang on 2020-04-02
+ */
+public class UserEntity {
+    private Integer id;
+    private String name;
+    private String roleName;
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
